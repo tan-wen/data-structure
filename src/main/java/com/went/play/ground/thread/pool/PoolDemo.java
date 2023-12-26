08fb38f3f05f02f7ea86e6dc3312f03b98156a87
tyron <872701869@qq.com>
2023-12-26 16:33:33 +0800

    多线程示例

--- src/main/java/com/went/play/ground/thread/pool/PoolDemo.java
+++ src/main/java/com/went/play/ground/thread/pool/PoolDemo.java
@@ -1,5 +1,8 @@
 package com.went.play.ground.thread.pool;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 import java.util.concurrent.*;
 
 /**
@@ -9,14 +12,87 @@ import java.util.concurrent.*;
  */
 public class PoolDemo {
 
-    ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+    private static final int TASK1_LIMIT = 10;
+    private static final int TASK2_LIMIT = 20;
+
+    static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
             2,
-            2,
-            0L,
-            TimeUnit.MILLISECONDS,
-            new SynchronousQueue<>(),
+            5,
+            1L,
+            TimeUnit.HOURS,
+            new LinkedBlockingDeque<>(2),
             Executors.defaultThreadFactory(),
-            new ThreadPoolExecutor.AbortPolicy()
+            new ThreadPoolExecutor.CallerRunsPolicy()
     );
 
+    public static void main(String[] args) {
+
+        CountDownLatch latch = new CountDownLatch(TASK1_LIMIT);
+
+        LinkedBlockingDeque<Runnable> deque = new LinkedBlockingDeque<>();
+        for (int i = 0; i < TASK1_LIMIT; i++) {
+            deque.push(new MyTask("work-" + i, latch));
+        }
+
+        for (int i = 0; i < TASK1_LIMIT; i++) {
+            Runnable runnable = deque.poll();
+            if (runnable == null) break;
+            threadPoolExecutor.execute(runnable);
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("任务1全部异步多线程执行完毕。开始执行下面的任务.....");
+
+        CountDownLatch latch2 = new CountDownLatch(TASK2_LIMIT);
+
+        for (int i = 0; i < TASK2_LIMIT; i++) {
+            threadPoolExecutor.execute(() -> {
+                System.out.println(Thread.currentThread() + "---任务2工作开始.....");
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(Thread.currentThread() + "---任务2完成工作");
+                latch2.countDown();
+            });
+        }
+
+        try {
+            latch2.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("任务2全部异步多线程执行完毕");
+
+        threadPoolExecutor.shutdown();
+
+    }
+
+}
+
+@Data
+@RequiredArgsConstructor
+class MyTask implements Runnable {
+
+    private final String name;
+
+    private final CountDownLatch latch;
+    @Override
+    public void run() {
+        try {
+            System.out.println(Thread.currentThread() + "---" + this.name + "任务1开始工作.....");
+            Thread.sleep(5000);
+            System.out.println(Thread.currentThread() + "---" + this.name + "任务1完成工作");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            latch.countDown();
+        }
+    }
 }
